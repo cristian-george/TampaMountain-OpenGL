758650257a313554830668bc57b4646239aeb758
Andrei278 <andreidruga1@gmail.com>
2022-05-07 19:51:48 +0300

    Added Vertex class.

---
+++ SMM-Relief/SMM-Relief/Vertex.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <glm.hpp>
+
+class Vertex
+{
+public:
+	Vertex() = default;
+
+	glm::vec3 Position;
+	glm::vec3 Normal;
+	glm::vec2 TextureCoords;
+
+	glm::vec3 Color;
+	glm::vec3 Ambient;
+	glm::vec3 Diffuse;
+	glm::vec3 Specular;
+
+	int ColorID = -1;
+};

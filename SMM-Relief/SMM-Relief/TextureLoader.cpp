9798b13d656d9f00e7fbae96e5383ec301ed17ad
Cristian George <cfieraru29@yahoo.com>
2022-05-09 21:22:19 +0300

    Added skybox texture loader.

--- SMM-Relief/SMM-Relief/TextureLoader.cpp
+++ SMM-Relief/SMM-Relief/TextureLoader.cpp
@@ -41,3 +41,34 @@ unsigned int CreateTexture(const std::string& strTexturePath)
 
 	return textureId;
 }
+
+unsigned int CreateSkyboxTexture(const std::vector<std::string>& skyboxFaces)
+{
+	unsigned int textureId;
+	glGenTextures(1, &textureId);
+	glBindTexture(GL_TEXTURE_CUBE_MAP, textureId);
+
+	int width, height, nrChannels;
+	for (unsigned int index = 0; index < skyboxFaces.size(); ++index)
+	{
+		unsigned char* data = stbi_load(skyboxFaces[index].c_str(), &width, &height, &nrChannels, 0);
+		if (data)
+		{
+			glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + index, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			stbi_image_free(data);
+		}
+		else
+		{
+			std::cout << "Cubemap texture failed to load at path: " << skyboxFaces[index] << std::endl;
+			stbi_image_free(data);
+		}
+	}
+
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+	return textureId;
+}
--- SMM-Relief/SMM-Relief/TextureLoader.h
+++ SMM-Relief/SMM-Relief/TextureLoader.h
@@ -1,7 +1,10 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 #include <stb_image.h>
 #include <GL/glew.h>
 
 unsigned int CreateTexture(const std::string& strTexturePath);
+
+unsigned int CreateSkyboxTexture(const std::vector<std::string>& skyboxFaces);
